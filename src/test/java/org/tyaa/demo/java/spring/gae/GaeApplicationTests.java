b76ce6de0d9467fc9c8fb9dce1d6091a938fbdcd
YuriiTrofimenko <tyaa@ukr.net>
2021-04-13 18:55:27 +0300

    init

---
+++ src/test/java/org/tyaa/demo/java/spring/gae/GaeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tyaa.demo.java.spring.gae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
